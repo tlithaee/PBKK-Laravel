969f2ce1618ce41219fef5829642f65110a4d1a4
tlithaee <syarifah.talithae@gmail.com>
2024-09-09 20:20:25 +0700

    initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <h1> Halaman Home </h1>
+    <a href="/"> Home </a>
+    <a href="/about"> About </a>
+    <a href="/blog"> Blog </a>
+    <a href="/contact"> Contact </a>
+    <h1> Halaman Contact </h1>
+    <ul>
+        <li>
+            <a href="instagram.com/tlithaee"> Instagram </a>
+        </li>
+    </ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
