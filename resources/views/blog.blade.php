4251beb665e1ba7a5d700270ecc7c7bcf70e6df1
tlithaee <syarifah.talithae@gmail.com>
2024-09-12 08:01:47 +0700

    feat: add view data

--- resources/views/blog.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-  <x-slot:title>
-    {{ $title }}
-  </x-slot:title>
-  <h3 class="text-l"> Ini adalah Blog Page </h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,38 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Lita',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque blanditiis ad atque, doloribus optio vel dolorem. Porro culpa suscipit veniam incidunt impedit repellat deserunt, quasi placeat excepturi iusto libero praesentium.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Lita',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque blanditiis ad atque, doloribus optio vel dolorem. Porro culpa suscipit veniam incidunt impedit repellat deserunt, quasi placeat excepturi iusto libero praesentium.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{id}', function($id){
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Lita',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque blanditiis ad atque, doloribus optio vel dolorem. Porro culpa suscipit veniam incidunt impedit repellat deserunt, quasi placeat excepturi iusto libero praesentium.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Lita',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque blanditiis ad atque, doloribus optio vel dolorem. Porro culpa suscipit veniam incidunt impedit repellat deserunt, quasi placeat excepturi iusto libero praesentium.'
+        ]
+    ];
 });
 
 Route::get('/contact', function () {
