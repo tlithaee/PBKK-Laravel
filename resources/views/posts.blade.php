8193646d7dd30d1a9cb3b6a44d459ecf41e59857
tlithaee <syarifah.talithae@gmail.com>
2024-12-16 11:51:45 +0700

    feat: pagination code

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -41,7 +41,9 @@
       </div>
   </div>
 
-  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-0">
+  {{ $posts->links() }}
+
+  <div class="my-4 py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-0">
     <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
       @forelse ($posts as $post)
         <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -97,5 +99,7 @@
       @endforelse
     </div>  
   </div>
+
+  {{ $posts->links() }}
 
 </x-layout>
